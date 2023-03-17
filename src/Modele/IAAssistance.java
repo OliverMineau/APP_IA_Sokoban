5f73811314c13f8c2697b728c9b5dd6af0fbc77f
OliverMineau <oliver.mineau42@orange.fr>
2023-03-17 15:44:23 +0100

    Graphe Perso et Caisses pas terminés

--- src/Modele/IAAssistance.java
+++ src/Modele/IAAssistance.java
@@ -27,9 +27,11 @@ package Modele;
  */
 
 import Global.Configuration;
+import Structures.Coords;
+import Structures.Graphe;
 import Structures.Sequence;
 
-import java.util.Random;
+import java.util.*;
 
 class IAAssistance extends IA {
 	Random r;
@@ -39,6 +41,12 @@ class IAAssistance extends IA {
 	}
 
 	//Fonction de départ de l'IA de l'APP ALGO6
+	/*
+	* Enlever les cases des coins et les couloirs impossibles
+	* Mettre dans une structure de graphe
+	* Faire le plus court chemin entre le joueur et une caisse
+	* Faire le plus court chemin entre la caisse et un but
+	* */
 	@Override
 	public Sequence<Coup> joue() {
 		Sequence<Coup> resultat = Configuration.nouvelleSequence();
@@ -60,6 +68,8 @@ class IAAssistance extends IA {
 				if (niveau.estOccupable(nouveauL, nouveauC)) {
 					Configuration.info("Téléportation en (" + nouveauL + ", " + nouveauC + ") !");
 					coup.deplacementPousseur(pousseurL, pousseurC, nouveauL, nouveauC);
+					//coup.ajouteMarque(pousseurL, pousseurC, 0xBB7755);
+
 					resultat.insereQueue(coup);
 					pousseurL = nouveauL;
 					pousseurC = nouveauC;
@@ -68,6 +78,104 @@ class IAAssistance extends IA {
 			}
 		}
 		Configuration.info("Sortie de la méthode de jeu de l'IA");
+
+		Niveau n = super.niveau;
+		ArrayList<Graphe> racinePerso = new ArrayList<>();
+		creationGraphePerso(racinePerso, n);
+
+		ArrayList<Graphe> racineCaisse = new ArrayList<>();
+		creationGrapheCaisse(racineCaisse, n);
+
+		showGraph(racinePerso);
+		System.out.println();
+		showGraph(racineCaisse);
+
+
 		return resultat;
+
+	}
+
+	public void creationGraphePerso(ArrayList<Graphe> racine, Niveau n){
+
+		for (int y = 0; y<n.lignes(); y++){
+			for (int x = 0; x<n.colonnes(); x++){
+				if(!n.aMur(y,x)){
+
+					ArrayList<Coords> adj = new ArrayList<>();
+					if(x-1 >= 0){
+						adj.add(new Coords(x-1,y));
+					}
+					if(x+1 < n.colonnes()){
+						adj.add(new Coords(x+1,y));
+					}
+					if(y-1 < n.lignes()){
+						adj.add(new Coords(x,y-1));
+					}
+					if(y-1 >= 0){
+						adj.add(new Coords(x,y+1));
+					}
+
+					racine.add(new Graphe(new Coords(x,y), adj));
+				}
+			}
+		}
+	}
+
+	public void creationGrapheCaisse(ArrayList<Graphe> racine, Niveau n){
+
+		for (int y = 0; y<n.lignes(); y++){
+			for (int x = 0; x<n.colonnes(); x++){
+				if(!n.aMur(y,x)){
+
+					ArrayList<Coords> adj = new ArrayList<>();
+					int c1,c2;
+					c1=c2=0;
+
+					// G
+					if(x-1 >= 0 && !n.aMur(y,x-1)){
+						adj.add(new Coords(x-1,y));
+					}else{
+						c1+=1;
+					}
+
+					// D
+					if(x+1 < n.colonnes() && !n.aMur(y,x+1)){
+						adj.add(new Coords(x+1,y));
+					}else{
+						c2+=1;
+					}
+
+					// B
+					if(y+1 < n.lignes() && !n.aMur(y+1,x)){
+						adj.add(new Coords(x,y+1));
+
+					}else{
+						c2+=1;
+						c1+=1;
+					}
+
+					// H
+					if(y-1 >= 0 && !n.aMur(y-1,x)){
+						adj.add(new Coords(x,y-1));
+					}else{
+						c1+=1;
+						c2+=1;
+					}
+
+					if((c1 < 2 && c2 < 2) || n.aBut(y,x)){
+						racine.add(new Graphe(new Coords(x,y), adj));
+					}
+				}
+			}
+		}
+	}
+
+
+	public void showGraph(ArrayList<Graphe> racine){
+		for(int i =0; i<racine.size(); i++){
+			System.out.format("Coords : x%d,y%d\n", racine.get(i).coords.x,racine.get(i).coords.y );
+		}
+		System.out.println("Taille graphe perso : " + racine.size());
+
 	}
 }
---
+++ src/Structures/Graphe.java
@@ -0,0 +1,20 @@
+package Structures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graphe {
+
+    public Coords coords;
+
+    public ArrayList<Coords> adj;
+
+    public Graphe(Coords coords , ArrayList<Coords> adj){
+        this.coords=coords;
+        this.adj=adj;
+    }
+
+
+
+
+}
